91dee6e70b0490407e2f5050fb1c17bb828893be
Kirill Dolgikh <kirill.dolgikh@saritasa.com>
2020-05-22 11:20:49 +0700

    Basic backend project with Note model based functionallity.

---
+++ gql/notes/types.py
@@ -0,0 +1,14 @@
+from graphene_django import DjangoObjectType
+from notes.models import Note
+
+
+class NoteType(DjangoObjectType):
+    class Meta:
+        model = Note
+        only_fields = (
+            'id',
+            'title',
+            'body',
+            'created_at',
+        )
+        use_connection = True
